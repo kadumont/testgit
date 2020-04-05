8e0d289a4f1162014e393575c767cbbe578c5052
Kaissy Dumont <luck.shang@protonmail.com>
2020-04-06 00:41:57 +0200

    Ajout Marabout simple 3 mots de plus

--- src/main/java/fr/unilim/iut/testgit/MaraboutSimple.java
+++ src/main/java/fr/unilim/iut/testgit/MaraboutSimple.java
@@ -2,6 +2,7 @@ package fr.unilim.iut.testgit;
 
 public class MaraboutSimple {
 	public static String getResult() {
-        return "Marabout, Bout de ficelle, Selle de cheval";
+        return "Marabout, Bout de ficelle, Selle de cheval"
+	              + ", Cheval de course, Course à pied, Pied à terre";
   }
 }
