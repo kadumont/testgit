f4d75d3259125bb242fa2b3985038c01387bc871
Kaissy Dumont <luck.shang@protonmail.com>
2020-04-06 00:08:39 +0200

    Suite FizzBuzz de 16 à 30

--- src/main/java/fr/unilim/iut/testgit/FizzBuzz.java
+++ src/main/java/fr/unilim/iut/testgit/FizzBuzz.java
@@ -1,7 +1,10 @@
 package fr.unilim.iut.testgit;
 
 public class FizzBuzz {
+	
 	public static String getResult() {
-	    return "1, 2, Fizz, 4, Buzz, Fizz, 7, 8, Fizz, Buzz, 11, Fizz, 13, 14, Fizz Buzz";
+	    return "1, 2, Fizz, 4, Buzz, Fizz, 7, 8, Fizz, Buzz, 11, Fizz, 13, 14, Fizz Buzz"
+			    + ", 16, 17, Fizz, 19, Buzz, Fizz, 22, 23, Fizz, Buzz, 26, Fizz, 28, 29, Fizz Buzz";
+
     }
 }
