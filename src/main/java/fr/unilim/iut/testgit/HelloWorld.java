aeb5d05644a2948a3689eef0259c4d5f620073f2
Kaissy Dumont <luck.shang@protonmail.com>
2020-04-05 20:00:15 +0200

    Nouvelle ligne

--- src/main/java/fr/unilim/iut/testgit/HelloWorld.java
+++ src/main/java/fr/unilim/iut/testgit/HelloWorld.java
@@ -3,5 +3,6 @@ package fr.unilim.iut.testgit;
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
+		System.out.println( "Jouons ensemble!" );
 	}
 }
