1047b7ee4f56bfaae771db5e818f8abd204053b0
Kaissy Dumont <luck.shang@protonmail.com>
2020-04-06 00:03:10 +0200

    Ajout Marabout 3 phrases

---
+++ src/main/java/fr/unilim/iut/testgit/Marabout.java
@@ -0,0 +1,11 @@
+package fr.unilim.iut.testgit;
+
+public class Marabout {
+	
+	public static String getResult() {
+
+	    return "Trois p'tits chats, trois p'tits chats, trois p'tits chats, chats, chats, \n"
+			   + "Chapeau d'paille, chapeau d'paille, chapeau d'paille, paille, paille, \n"
+			   + "Paillasson, paillasson, paillasson, -son, -son,";
+     }
+}
