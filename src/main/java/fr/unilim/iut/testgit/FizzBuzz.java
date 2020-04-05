e91db96b97563cf8c80446d498e94fa95446eb4c
Kaissy Dumont <luck.shang@protonmail.com>
2020-04-05 22:41:16 +0200

    Ajout FizzBuzz de 1 à 15

---
+++ src/main/java/fr/unilim/iut/testgit/FizzBuzz.java
@@ -0,0 +1,7 @@
+package fr.unilim.iut.testgit;
+
+public class FizzBuzz {
+	public static String getResult() {
+	    return "1, 2, Fizz, 4, Buzz, Fizz, 7, 8, Fizz, Buzz, 11, Fizz, 13, 14, Fizz Buzz";
+    }
+}
