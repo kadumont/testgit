993cb4ae52095ee9595d6fda543206af6706d895
Kaissy Dumont <luck.shang@protonmail.com>
2020-04-06 00:05:27 +0200

    Suite Marabout avec 3 phrases de plus

--- src/main/java/fr/unilim/iut/testgit/Marabout.java
+++ src/main/java/fr/unilim/iut/testgit/Marabout.java
@@ -2,10 +2,13 @@ package fr.unilim.iut.testgit;
 
 public class Marabout {
 	
-	public static String getResult() {
+    public static String getResult() {
 
 	    return "Trois p'tits chats, trois p'tits chats, trois p'tits chats, chats, chats, \n"
-			   + "Chapeau d'paille, chapeau d'paille, chapeau d'paille, paille, paille, \n"
-			   + "Paillasson, paillasson, paillasson, -son, -son,";
+			+ "Chapeau d'paille, chapeau d'paille, chapeau d'paille, paille, paille, \n"
+			+ "Paillasson, paillasson, paillasson, -son, -son, \n"
+			+ "Somnambule, somnambule, somnambule, -bule, -bule, \n"
+			+ "Bulletin, bulletin, bulletin, -tin, -tin, \n"
+			+ "Tintamarre, tintamarre, tintamarre, -marre, -marre,";
      }
 }
